ff145f5abbc937955bf1f4c819ab96adf1809797
Filipe Maia <filipe.c.maia@gmail.com>
2011-01-29 23:29:35 -0800

    Try to fix the slice insert. Makes it CC2.0 or above

--- emc_cuda.cu
+++ emc_cuda.cu
@@ -52,8 +52,11 @@ __device__ void cuda_insert_slice(real *model, real *weight, real *slice,
 	if (round_x >= 0 && round_x < model_x &&
 	    round_y >= 0 && round_y < model_y &&
 	    round_z >= 0 && round_z < model_z) {
-	  model[(int)(round_z*model_x*model_y + round_y*model_x + round_x)] += w * slice[y+x_max + x];	    
-	  weight[(int)(round_z*model_x*model_y + round_y*model_x + round_x)] += w;
+	  /* this can cause problems due to non atomic operations */
+	  //	  model[(int)(round_z*model_x*model_y + round_y*model_x + round_x)] += w * slice[y+x_max + x];	    
+	  atomicAdd(&model[(int)(round_z*model_x*model_y + round_y*model_x + round_x)], w * slice[y+x_max + x]);
+	  //	  weight[(int)(round_z*model_x*model_y + round_y*model_x + round_x)] += w;
+	  atomicAdd(&weight[(int)(round_z*model_x*model_y + round_y*model_x + round_x)], w);
 	}
       }//endif
     }
@@ -262,7 +265,7 @@ __global__ void update_slices_kernel(real * images, real * slices, int * mask, r
 				     real * slices_total_respons, real * rot,
 				     real * x_coord, real * y_coord, real * z_coord,
 				     real * model, real * weight,
-				     real w, int slice_rows, int slice_cols,
+				     int slice_rows, int slice_cols,
 				     int model_x, int model_y, int model_z, real * weights){
   /* each block takes care of 1 slice */
   int bid = blockIdx.x;
@@ -292,11 +295,11 @@ __global__ void update_slices_kernel(real * images, real * slices, int * mask, r
 	slices[i_slice*N_2d+i] /= total_respons;
       }
     }
+    cuda_insert_slice(model,weight,&slices[bid*N_2d],mask,weights[bid]*total_respons,
+		      &rot[4*bid],x_coord,y_coord,z_coord,
+		      slice_rows,slice_cols,(real)model_x,(real)model_y,(real)model_z,tid,step);
   }
   
-  cuda_insert_slice(model,weight,&slices[bid*N_2d],mask,weights[bid]*total_respons,
-		    &rot[4*bid],x_coord,y_coord,z_coord,
-		    slice_rows,slice_cols,(real)model_x,(real)model_y,(real)model_z,tid,step);
 }
 
 real cuda_update_slices(sp_matrix ** images, sp_matrix ** slices, sp_imatrix * mask,
@@ -333,11 +336,11 @@ real cuda_update_slices(sp_matrix ** images, sp_matrix ** slices, sp_imatrix * m
 
   real * d_model;
   cudaMalloc(&d_model,sizeof(real)*sp_3matrix_size(model));
-  cudaMemcpy(d_model,model,sizeof(real)*sp_3matrix_size(model),cudaMemcpyHostToDevice);
+  cudaMemcpy(d_model,model->data,sizeof(real)*sp_3matrix_size(model),cudaMemcpyHostToDevice);
 
   real * d_weight;
   cudaMalloc(&d_weight,sizeof(real)*sp_3matrix_size(weight));
-  cudaMemcpy(d_weight,weight,sizeof(real)*sp_3matrix_size(weight),cudaMemcpyHostToDevice);
+  cudaMemcpy(d_weight,weight->data,sizeof(real)*sp_3matrix_size(weight),cudaMemcpyHostToDevice);
 
   real * d_rot;
   cudaMalloc(&d_rot,sizeof(real)*N_slices*4);
@@ -347,15 +350,15 @@ real cuda_update_slices(sp_matrix ** images, sp_matrix ** slices, sp_imatrix * m
 
   real * d_x_coordinates;
   cudaMalloc(&d_x_coordinates,sizeof(real)*sp_matrix_size(x_coordinates));
-  cudaMemcpy(d_x_coordinates,x_coordinates,sizeof(real)*sp_matrix_size(x_coordinates),cudaMemcpyHostToDevice);
+  cudaMemcpy(d_x_coordinates,x_coordinates->data,sizeof(real)*sp_matrix_size(x_coordinates),cudaMemcpyHostToDevice);
 
   real * d_y_coordinates;
   cudaMalloc(&d_y_coordinates,sizeof(real)*sp_matrix_size(y_coordinates));
-  cudaMemcpy(d_y_coordinates,y_coordinates,sizeof(real)*sp_matrix_size(y_coordinates),cudaMemcpyHostToDevice);
+  cudaMemcpy(d_y_coordinates,y_coordinates->data,sizeof(real)*sp_matrix_size(y_coordinates),cudaMemcpyHostToDevice);
 
   real * d_z_coordinates;
   cudaMalloc(&d_z_coordinates,sizeof(real)*sp_matrix_size(z_coordinates));
-  cudaMemcpy(d_z_coordinates,z_coordinates,sizeof(real)*sp_matrix_size(z_coordinates),cudaMemcpyHostToDevice);
+  cudaMemcpy(d_z_coordinates,z_coordinates->data,sizeof(real)*sp_matrix_size(z_coordinates),cudaMemcpyHostToDevice);
 
 
   real * d_weights;
@@ -373,7 +376,7 @@ real cuda_update_slices(sp_matrix ** images, sp_matrix ** slices, sp_imatrix * m
   update_slices_kernel<<<nblocks,nthreads>>>(d_images, d_slices, d_mask, d_respons,
 					     d_scaling, N_images, N_slices, N_2d,
 					     d_slices_total_respons, d_rot,d_x_coordinates,
-					     d_y_coordinates,d_z_coordinates,d_model, d_weight,1.0,
+					     d_y_coordinates,d_z_coordinates,d_model, d_weight,
 					     sp_matrix_rows(slices[0]),sp_matrix_cols(slices[0]),
 					     sp_3matrix_x(model),sp_3matrix_y(model),
 					     sp_3matrix_z(model),d_weights);  
@@ -405,8 +408,8 @@ real cuda_update_slices(sp_matrix ** images, sp_matrix ** slices, sp_imatrix * m
 		   rotations[i_slice], x_coordinates, y_coordinates, z_coordinates);*/
     }
   }
-  cudaMemcpy(model->data,d_model,sizeof(real)*sp_3matrix_size(model),cudaMemcpyDeviceToHost);
-  cudaMemcpy(weight->data,d_weight,sizeof(real)*sp_3matrix_size(model),cudaMemcpyDeviceToHost);
+    cudaMemcpy(model->data,d_model,sizeof(real)*sp_3matrix_size(model),cudaMemcpyDeviceToHost);
+    cudaMemcpy(weight->data,d_weight,sizeof(real)*sp_3matrix_size(model),cudaMemcpyDeviceToHost);
 
   cudaFree(d_images);
   cudaFree(d_slices);
